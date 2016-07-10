6247a9c74b5ea44dbfa719ccd9ac6d09784497a9
Eric Kryski <e.kryski@gmail.com>
2016-07-10 01:42:05 -0600

    splitting up the generate command

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -4,11 +4,10 @@
  */
 
 import path from 'path';
-import chalk from 'chalk';
 import inquirer from 'inquirer';
 import { existsSync as exists } from 'fs';
 import merge from 'lodash.merge';
-import Generator from 'feathers-generator';
+import generate from '../utils/generate';
 
 export default function(program) {
   program
@@ -33,41 +32,13 @@ export default function(program) {
     .action((template, name, command) => {
       const DEFAULTS = {
         template: 'app',
-        name: path.relative('..', process.cwd()),
         path: '.',
         force: false
       };
 
       let args = merge(DEFAULTS, { template, name, force: command.force, path: command.path });
       args.root = path.resolve(args.path);
-      
-      const run = function() {
-        program.debug(`Running '${args.template}' generator with options`, args);
-        program.debug(`Using directory: ${args.root}`);
-        
-        const generator = Generator(args);
-
-        generator.getQuestions()
-          .then(questions => {
-            // Get user to answer questions
-            return inquirer.prompt(questions);
-          })
-          .then(answers => {
-            // Send answers back to generator
-            return generator.generate(answers);
-          })
-          .then(dependencies => {
-            // npm install dependencies
-            // { devDependencies: [], dependencies: [] }
-          })
-          .catch(error => {
-            if (error) {
-              console.log(chalk.red('ERROR', error));
-            }
-
-            process.exit(1);
-          });
-      }
+      args.name = args.name || path.parse(args.root).name;
 
       if (exists(args.root) && !args.force) {
         inquirer.prompt([{
@@ -77,16 +48,12 @@ export default function(program) {
         }])
         .then(answers => {
           if (answers.ok) {
-            run();
+            generate.bind(program)(args);
           }
         });
       }
       else {
-        // We are using a new directory so use the root directory
-        // name as the default name.
-        args.name = path.parse(args.root).name;
-
-        run();
+        generate.bind(program)(args);
       }
     });
 }
---
+++ src/utils/generate.js
@@ -0,0 +1,32 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import Generator from 'feathers-generator';
+
+export default function(args) {
+  const program = this;
+  const generator = Generator(args);
+
+  program.debug(`Running '${args.template}' generator with options`, args);
+  program.debug(`Using directory: ${args.root}`);
+
+  generator.getQuestions()
+    .then(questions => {
+      // Get user to answer questions
+      return inquirer.prompt(questions);
+    })
+    .then(answers => {
+      // Send answers back to generator
+      return generator.generate(answers);
+    })
+    .then(dependencies => {
+      // npm install dependencies
+      // { devDependencies: [], dependencies: [] }
+    })
+    .catch(error => {
+      if (error) {
+        console.log(chalk.red('ERROR', error));
+      }
+
+      process.exit(1);
+    });
+}
