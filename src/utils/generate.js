42c538b0c8af38ca2e5b785f4d3291651dc626e7
Eric Kryski <e.kryski@gmail.com>
2016-07-12 22:44:40 -0600

    changing generate command to support new generator structure

--- src/utils/generate.js
+++ src/utils/generate.js
@@ -1,33 +1,34 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import Generator from 'feathers-generator';
+import generator from 'feathers-generator';
 
 export default function(args) {
   const program = this;
-  const generator = Generator(args);
+
+  const errorHandler = function(error) {
+    console.log(chalk.red('ERROR', error));
+    process.exit(1);
+  };
+
+  const prompt = function(error, questions, callback) {
+    if (error) {
+      return errorHandler(error);
+    }
+
+    inquirer.prompt(questions).then(callback).catch(errorHandler);
+  };
+
+  const done = function(error, message) {
+    if (error) {
+      return errorHandler(error);
+    }
+    
+    message = message || 'Success!';
+    console.log(chalk.green(message));    
+  };
 
   program.debug(`Running '${args.template}' generator with options`, args);
   program.debug(`Using directory: ${args.root}`);
 
-  generator.getQuestions()
-    .then(questions => {
-      // Get user to answer questions
-      return inquirer.prompt(questions);
-    })
-    .then(answers => {
-      // Send answers back to generator
-      return generator.generate(answers);
-    })
-    .then(message => {
-      message = message || 'Success!';
-      
-      console.log(chalk.green(message));
-    })
-    .catch(error => {
-      if (error) {
-        console.log(chalk.red('ERROR', error));
-      }
-
-      process.exit(1);
-    });
+  generator(prompt, done, args);
 }
