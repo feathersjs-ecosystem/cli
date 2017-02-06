2dc0fb64477d801ef397ced958d68653d0b37c93
Eric Kryski <e.kryski@gmail.com>
2017-02-06 04:11:38 -0700

    adding more tests for the generate command

--- test/commands/generate.test.js
+++ test/commands/generate.test.js
@@ -3,13 +3,32 @@ import generate from '../../src/commands/generate';
 import program from 'commander';
 
 describe('command:generate', () => {
-  it('registers the command', () => {
-    generate(program);
+  let command;
 
-    const command = program.commands
+  before(() => {
+    generate(program);
+    command = program.commands
       .filter(c => c.name() === 'generate')
       .reduce(c => !!c);
+  });
 
+  it('registers the command', () => {
     expect(command).to.not.equal(undefined);
+  });
+
+  it('has force option', () => {
+    const option = command.options
+      .filter(o => o.long === '--force')
+      .reduce(o => !!o);
+
+    expect(option).to.not.equal(undefined);
+  });
+
+  it('has a path option', () => {
+    const option = command.options
+      .filter(o => o.long === '--path')
+      .reduce(o => !!o);
+
+    expect(option).to.not.equal(undefined);
   });
 });
