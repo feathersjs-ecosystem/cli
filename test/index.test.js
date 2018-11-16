134b96da2e601c9b2bbec9ee4bb3f8e4c831ab9a
David Luecke <daff@neyeon.com>
2018-11-15 16:44:27 -0800

    Use strictEqual

--- test/index.test.js
+++ test/index.test.js
@@ -5,19 +5,19 @@ const cli = require('../lib');
 
 describe('@feathersjs/cli', () => {
   it('is CommonJS compatible', () => {
-    assert.strict.equal(typeof require('../lib'), 'function');
+    assert.strictEqual(typeof require('../lib'), 'function');
   });
 
   it('basic functionality', () => {
-    assert.strict.equal(typeof cli, 'function', 'It worked');
+    assert.strictEqual(typeof cli, 'function', 'It worked');
   });
 
   it('runs the program with `generate` argument', function (done) {
     this.timeout(5000);
 
     exec(`node ${path.join(__dirname, '../bin/feathers.js')} generate`, (err, stdout, stderr) => {
-      assert.strict.equal(err, null);
-      assert.strict.equal(stderr, '');
+      assert.strictEqual(err, null);
+      assert.strictEqual(stderr, '');
       assert.ok(
         /Usage: feathers generate \[type\]/.test(stdout),
         'stdout should contain "Usage: feathers generate [type]"'
