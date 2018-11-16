a7a078cbab62d4bae28b20f72ccd5a142e0dffbf
David Luecke <daff@neyeon.com>
2018-11-15 16:28:33 -0800

    Back merge with changes from monorepo to put back into own repository

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const { exec } = require('child_process');
+const path = require('path');
 const cli = require('../lib');
 
 describe('@feathersjs/cli', () => {
@@ -6,12 +8,22 @@ describe('@feathersjs/cli', () => {
     assert.equal(typeof require('../lib'), 'function');
   });
 
-  it('basic functionality', done => {
+  it('basic functionality', () => {
     assert.equal(typeof cli, 'function', 'It worked');
-    done();
   });
 
-  it('runs the program with `generate` argument', () => {
-    cli([ null, __filename, 'generate' ]);
+  it('runs the program with `generate` argument', function (done) {
+    this.timeout(5000);
+
+    exec(`node ${path.join(__dirname, '../bin/feathers.js')} generate`, (err, stdout, stderr) => {
+      assert.equal(err, null);
+      assert.equal(stderr, '');
+      assert.ok(
+        /Usage: feathers generate \[type\]/.test(stdout),
+        'stdout should contain "Usage: feathers generate [type]"'
+      );
+
+      done();
+    });
   });
 });
