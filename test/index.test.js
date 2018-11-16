2e632d507206c8ee3eabf7000372f6cc6f812b44
David Luecke <daff@neyeon.com>
2018-11-15 16:34:54 -0800

    Update dependencies and package-lock.json

--- test/index.test.js
+++ test/index.test.js
@@ -5,19 +5,19 @@ const cli = require('../lib');
 
 describe('@feathersjs/cli', () => {
   it('is CommonJS compatible', () => {
-    assert.equal(typeof require('../lib'), 'function');
+    assert.strict.equal(typeof require('../lib'), 'function');
   });
 
   it('basic functionality', () => {
-    assert.equal(typeof cli, 'function', 'It worked');
+    assert.strict.equal(typeof cli, 'function', 'It worked');
   });
 
   it('runs the program with `generate` argument', function (done) {
     this.timeout(5000);
 
     exec(`node ${path.join(__dirname, '../bin/feathers.js')} generate`, (err, stdout, stderr) => {
-      assert.equal(err, null);
-      assert.equal(stderr, '');
+      assert.strict.equal(err, null);
+      assert.strict.equal(stderr, '');
       assert.ok(
         /Usage: feathers generate \[type\]/.test(stdout),
         'stdout should contain "Usage: feathers generate [type]"'
